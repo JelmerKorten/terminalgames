2f13830d03eb26cb1656fa633f55e0e36ce72670
JelmerKorten <jelmer.korten@gmail.com>
2023-08-04 13:55:27 +0400

    start camelrace

---
+++ the-casino/camelrace.py
@@ -0,0 +1,5 @@
+# using turtle to create a camel race that can be bet on.
+# no controls to "help" the camel yet.
+
+import turtle
+
--- the-casino/test.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-df = pd.DataFrame.from_dict({"lat":[-10.1,-11.1,10.1], "lon":[-38.1,-38.1,-38.1]})
-df = df.loc[(df["lat"]== -10.1) & (df["lon"] == -38.1)]
-print(df.dtypes)
