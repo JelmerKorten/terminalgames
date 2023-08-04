053001084d583fd9f90eb44e616ad7b239721f1f
JelmerKorten <jelmer.korten@gmail.com>
2023-08-04 14:05:46 +0400

    init turtle

--- the-casino/camelrace.py
+++ the-casino/camelrace.py
@@ -1,5 +1,21 @@
 # using turtle to create a camel race that can be bet on.
 # no controls to "help" the camel yet.
 
-import turtle
+import turtle as t
+import time
 
+WIDTH, HEIGHT = 600, 600
+
+t.screensize(WIDTH,HEIGHT)
+arr1 = t.Turtle()
+arr2 = t.Turtle()
+
+for _ in range(4):
+    arr1.forward(50)
+    arr1.left(90)
+    arr1.forward(50)
+    arr1.left(90)
+    arr2.forward(50)
+    arr2.left(90)
+
+time.sleep(5)
